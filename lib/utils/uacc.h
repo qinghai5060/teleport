76aab1d4183f54e8b2382cebc49b1799e6f1e083
Acrimon <joel.wejdenstal@gmail.com>
2021-02-05 08:11:26 +0100

    simplify some code

--- lib/utils/uacc.h
+++ lib/utils/uacc.h
@@ -50,11 +50,7 @@ static int uacc_add_utmp_entry(char *username, char *hostname, int32_t remote_ad
     memcpy(&entry.ut_addr_v6, &remote_addr_v6, sizeof(int32_t) * 4);
     setutent();
     if (pututline(&entry) == NULL) {
-        if (errno == EPERM) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return errno == EPERM ? 2 : 1;
     }
     endutent();
     updwtmp(_PATH_WTMP, &entry);
@@ -77,11 +73,7 @@ static int uacc_mark_utmp_entry_dead(char *tty_name) {
     entry.ut_type = DEAD_PROCESS;
     setutent();
     if (pututline(&entry) == NULL) {
-        if (errno == EPERM) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return errno == EPERM ? 2 : 1;
     }
     endutent();
     updwtmp(_PATH_WTMP, &entry);
