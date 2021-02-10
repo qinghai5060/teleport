51ca8d52855be7794d778a1ea3d978a4f9b5dac4
Acrimon <joel.wejdenstal@gmail.com>
2021-02-10 22:20:47 +0100

    refactored and made the utmp integration test runnable

--- integration/utmp_integration_test.go
+++ integration/utmp_integration_test.go
@@ -1,4 +1,4 @@
-// +build root_integration
+//+build test_as_root
 
 /*
 Copyright 2021 Gravitational, Inc.
@@ -20,16 +20,26 @@ package integration
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"testing"
 	"time"
 
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/lib/auth"
+	"github.com/gravitational/teleport/lib/bpf"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/pam"
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/srv/regular"
 	"github.com/gravitational/teleport/lib/srv/uacc"
+	"github.com/gravitational/teleport/lib/sshutils"
+	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/trace"
+	"github.com/jonboulle/clockwork"
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/crypto/ssh"
-	"gopkg.in/check.v1"
 	. "gopkg.in/check.v1"
 )
 
@@ -41,19 +51,22 @@ var wildcardAllow = services.Labels{
 	services.Wildcard: []string{services.Wildcard},
 }
 
-var _ = check.Suite(&SrvSuite{})
-
-type SrvSuite struct {
+type SrvCtx struct {
 	srv        *regular.Server
 	signer     ssh.Signer
 	server     *auth.TestTLSServer
 	testServer *auth.TestAuthServer
+	clock      clockwork.FakeClock
+	nodeClient *auth.Client
+	nodeID     string
 }
 
 // TestUTMP tests that user accounting is done on supported systems.
-func (s *SrvSuite) TestUTMPEntryExists(c *C) {
+func TestUTMPEntryExists(t *testing.T) {
+	s := &SrvCtx{}
+	s.SetUpContext(t)
 	up, err := s.newUpack(teleportTestUser, []string{teleportTestUser}, wildcardAllow)
-	c.Assert(err, IsNil)
+	require.NoError(t, err)
 
 	sshConfig2 := &ssh.ClientConfig{
 		User:            teleportTestUser,
@@ -62,20 +75,20 @@ func (s *SrvSuite) TestUTMPEntryExists(c *C) {
 	}
 
 	client, err := ssh.Dial("tcp", s.srv.Addr(), sshConfig2)
-	c.Assert(err, IsNil)
+	require.NoError(t, err)
 	defer func() {
 		err := client.Close()
-		c.Assert(err, IsNil)
+		require.NoError(t, err)
 	}()
 
 	se, err := client.NewSession()
-	c.Assert(err, IsNil)
+	require.NoError(t, err)
 	defer se.Close()
 
-	c.Assert(se.RequestPty("xterm", 30, 30, ssh.TerminalModes{}), IsNil)
+	require.NoError(t, se.RequestPty("xterm", 30, 30, ssh.TerminalModes{}), nil)
 	entryExists, err := uacc.UserWithPtyInDatabase(teleportTestUser)
-	c.Assert(err, IsNil)
-	c.Assert(entryExists, Equals, true)
+	require.NoError(t, err)
+	require.Equal(t, entryExists, true)
 }
 
 // upack holds all ssh signing artefacts needed for signing and checking user keys
@@ -102,7 +115,80 @@ type upack struct {
 	certSigner ssh.Signer
 }
 
-func (s *SrvSuite) newUpack(username string, allowedLogins []string, allowedLabels services.Labels) (*upack, error) {
+const hostID = "00000000-0000-0000-0000-000000000000"
+
+func (s *SrvCtx) SetUpContext(t *testing.T) {
+	s.clock = clockwork.NewFakeClock()
+	tempdir, err := ioutil.TempDir("", "utmp-integration")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	authServer, err := auth.NewTestAuthServer(auth.TestAuthServerConfig{
+		ClusterName: "localhost",
+		Dir:         tempdir,
+		Clock:       s.clock,
+	})
+	require.NoError(t, err)
+	s.server, err = authServer.NewTestTLSServer()
+	require.NoError(t, err)
+	s.testServer = authServer
+
+	// set up host private key and certificate
+	certs, err := s.server.Auth().GenerateServerKeys(auth.GenerateServerKeysRequest{
+		HostID:   hostID,
+		NodeName: s.server.ClusterName(),
+		Roles:    teleport.Roles{teleport.RoleNode},
+	})
+	require.NoError(t, err)
+
+	// set up user CA and set up a user that has access to the server
+	s.signer, err = sshutils.NewSigner(certs.Key, certs.Cert)
+	require.NoError(t, err)
+
+	s.nodeID = uuid.New()
+	s.nodeClient, err = s.server.NewClient(auth.TestIdentity{
+		I: auth.BuiltinRole{
+			Role:     teleport.RoleNode,
+			Username: s.nodeID,
+		},
+	})
+	require.NoError(t, err)
+
+	nodeDir, err := ioutil.TempDir("", "utmp-integration")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+	srv, err := regular.New(
+		utils.NetAddr{AddrNetwork: "tcp", Addr: "127.0.0.1:0"},
+		s.server.ClusterName(),
+		[]ssh.Signer{s.signer},
+		s.nodeClient,
+		nodeDir,
+		"",
+		utils.NetAddr{},
+		regular.SetUUID(s.nodeID),
+		regular.SetNamespace(defaults.Namespace),
+		regular.SetEmitter(s.nodeClient),
+		regular.SetShell("/bin/sh"),
+		regular.SetSessionServer(s.nodeClient),
+		regular.SetPAMConfig(&pam.Config{Enabled: false}),
+		regular.SetLabels(
+			map[string]string{"foo": "bar"},
+			services.CommandLabels{
+				"baz": &services.CommandLabelV2{
+					Period:  services.NewDuration(time.Millisecond),
+					Command: []string{"expr", "1", "+", "3"}},
+			},
+		),
+		regular.SetBPF(&bpf.NOP{}),
+		regular.SetClock(s.clock),
+	)
+	require.NoError(t, err)
+	s.srv = srv
+	require.NoError(t, auth.CreateUploaderDir(nodeDir), IsNil)
+	require.NoError(t, s.srv.Start())
+}
+
+func (s *SrvCtx) newUpack(username string, allowedLogins []string, allowedLabels services.Labels) (*upack, error) {
 	ctx := context.Background()
 	auth := s.server.Auth()
 	upriv, upub, err := auth.GenerateKeyPair("")
